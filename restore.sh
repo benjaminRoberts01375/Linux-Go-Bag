df974937ae25c763428fcc27fc33b9290b1d2767
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-06-10 17:21:32 -0400

    Allow split wallpapers

--- restore.sh
+++ restore.sh
@@ -72,7 +72,8 @@ cp -r blender ~/.config/
 cp -r rofi ~/.config/
 cp -r .supermaven ~/
 cp -r fonts ~/.local/share/
-cp wallpaper.jpg ~/Documents/wallpaper.jpg
+cp wallpaper-left.jpg ~/Documents/wallpaper-left.jpg
+cp wallpaper-right.jpg ~/Documents/wallpaper-right.jpg
 cp .bash_history ~/.bash_history
 sudo cp -r waybar /etc/xdg/
 cp -r hypr ~/.config/
--- save.sh
+++ save.sh
@@ -17,7 +17,8 @@ cp -r ~/.config/warp-terminal warp-terminal
 cp /etc/pacman.conf pacman.conf
 
 # Copy data
-cp ~/Documents/wallpaper.jpg wallpaper.jpg
+cp ~/Documents/wallpaper-left.jpg wallpaper-left.jpg
+cp ~/Documents/wallpaper-right.jpg wallpaper-right.jpg
 cp ~/.bash_history .bash_history
 cp -r ~/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances instances
 cp -r ~/.local/share/fonts fonts
