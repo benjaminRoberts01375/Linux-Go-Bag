b30a9a8213089b18821f598d2bbd568002e1d2ce
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-06-10 23:14:07 -0400

    Remove dolphin files

--- restore.sh
+++ restore.sh
@@ -92,6 +92,7 @@ gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
 # Remove unneeded packages
 sudo pacman -Rsn $(pacman -Qdtq)
+rm -rf ~/.local/share/dolphin || true
 
 # Setup permissions
 sudo chmod +x /etc/xdg/waybar/cpu_vbar.py
